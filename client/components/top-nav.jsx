d62b7ba22ce7e960c05dce6e64762cb31aa8400c
sebtedesco <stedescoc@gmail.com>
2020-04-01 00:17:19 +0000

    added top nav items

--- client/components/top-nav.jsx
+++ client/components/top-nav.jsx
@@ -5,8 +5,25 @@ export default class TopNav extends React.Component {
 
   render() {
     return (
-      <div className="top-nav">
-        {/* <div className="left-box"></div>
+      <div className="fixed-top top-nav d-flex justify-content-between align-items-end">
+        <a className="initials d-flex justify-content-center align-items-end" href="#">
+          <h2>ST</h2>
+        </a>
+        <nav>
+          <ul>
+            <li><a href="#"></a>About</li>
+            <li><a href="#"></a>Skills</li>
+            <li><a href="#"></a>Tools</li>
+            <li><a href="#"></a>Portfolio</li>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+}
+
+
+{/* <div className="left-box"></div>
         <div className="right-box"></div>
         <nav className="navbar">
           <ul>
@@ -17,7 +34,3 @@ export default class TopNav extends React.Component {
             <li className="nav-item">Contact</li>
           </ul>
         </nav> */}
-      </div>
-    )
-  }
-}
